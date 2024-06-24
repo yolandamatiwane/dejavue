d40f4d69f7de1170bccb6673b00e46963a17c4ec
yolandamatiwane <mbalentle.matiwane21@gmail.com>
2024-06-24 14:45:54 +0200

    made nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FriendsView from '../views/FriendsView.vue'
+import CarsView from '../views/CarsView.vue'
+import ConsolesView from '../views/ConsolesView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path:'/friends',
+    name:'friends',
+    component: FriendsView
+  },
+  {
+    path:'/cars',
+    name:'cars',
+    component: CarsView
+  },
+  {
+    path:'/consoles',
+    name:'consoles',
+    component: ConsolesView
   },
   {
     path: '/about',
